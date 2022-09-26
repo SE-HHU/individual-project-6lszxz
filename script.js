05a8cd84c03de181697b3749f6109cb212a2beea
6lszxz <ediamonds@126.com>
2022-09-26 13:00:31 +0800

    添加了三个数的运算以及一键下载

--- script.js
+++ script.js
@@ -1,61 +1,109 @@
 /**
- *
- * @constructor
- * @description 一个Question类，包括两个数a,b，运算符operation和结果result，operator为0代表加，为1代表减
+ * @author 6lszxz <2106050119@hhu.edu.cn>
+ * @function
+ * Question类，一个有若干操作符和操作数的算式
  */
 function Question(){
-    let opJudge=Math.floor(Math.random()*2);
-    if(opJudge===0){
-        this.operator ='+';
-    }else {
-        this.operator ='-';
-    }
-    this.a= Math.floor(Math.random()*100);
-    this.b= Math.floor(Math.random()*100);
-    if(this.operator ==='-'){
-        while(this.a-this.b<0){
-            this.b= Math.floor(Math.random()*100);
-        }
-    }else {
-        while (this.a+this.b>100){
-            this.b=Math.floor(Math.random()*100);
+    /**
+     * @type {number}
+     * 操作数的个数
+     */
+    this.size = Math.floor(Math.random()*2+2);
+    let operators =[];
+    for(let i =0;i<this.size-1;i++){
+        let opJudge=Math.floor(Math.random()*2);
+        if(opJudge===0){
+            operators.push("+");
+        }else {
+            operators.push("-");
         }
     }
-    switch (this.operator){
-        case '+':
-            this.result = this.a + this.b;
-            break;
-        case "-":
-            this.result = this.a - this.b;
-            break;
-    }
-    this.toString =function (){
-        return String(this.a)+String(this.operator)+String(this.b) +"=";
-    }
-}
 
-/**
- *
- * @param A 算式A
- * @param B 算式B
- * @returns {boolean} 相等为true，不相等为false
- * @description 判断两个算式是否是相等的算式
- */
-function isTwoQuestionsEqual(A,B)
-{
-    if(A.operator!==B.operator){
+    /**
+     * @type {*[]}
+     * 操作符数组，大小为size-1
+     */
+    this.operators = operators;
+    let numbers = [];
+    for (let i = 0; i < this.size; i++) {
+        numbers.push(Math.floor(Math.random() * 100));
+    }
+
+    /**
+     * @type {*[]}
+     * 操作数数组，大小为size
+     */
+    this.numbers = numbers;
+
+    /**
+     * @type {*[]}
+     * 每个子集的结果
+     */
+    this.partResults =[];
+
+    /**
+     * @returns {boolean}
+     * 检测是否超纲，超纲为true，不超纲为false
+     */
+    this.isOutOfEdge = function (){
+        let result =0;
+        for(let i=0;i<this.size;i++){
+            if(i===0){
+                result+=this.numbers[i];
+                this.partResults[i] = result;
+                continue;
+            }
+            if(this.operators[i-1]==='+'){
+                result+=this.numbers[i];
+            }else {
+                result -= this.numbers[i];
+            }
+            this.partResults[i] = result;
+        }
+        this.totalResult=result;
+        for (let partResult of this.partResults) {
+            if(partResult<0 || partResult>100){
+                return true;
+            }
+        }
         return false;
     }
-    if(A.operator ===1){
-        return A.a === B.a && A.b === B.b;
-    }else {
-        return (A.a === B.a && A.b === B.b) || (A.b === B.a && A.a === B.b);
+
+    /**
+     * @returns {string}
+     * 重写toString方法
+     */
+    this.toString= function (){
+        let result ="";
+        for(let i=0;i<this.size;i++){
+            if(i!==0){
+                result+= this.operators[i-1];
+            }
+            result+= String(this.numbers[i]);
+        }
+        return result;
     }
 }
 
 /**
- * @description 生成一定数量的算式，返回一个Question数组。数量由输入框中的参数给出。
- * @returns {null|*[]} 返回的数组
+ * @function
+ * 判断两个算式是否相等
+ * @param A
+ * 算式A
+ * @param B
+ * 算式B
+ * @returns {boolean}
+ * 两个算式是否相等，相等为true，不相等为false
+ */
+function isTwoQuestionsEqual(A,B) {
+    return String(A.operators) === String(B.operators) && String(A.numbers) === String(B.numbers)
+}
+
+/**
+ * @function
+ * 生成一定数量的算式，返回一个Question数组。数量由输入框中的参数给出。
+ * @returns {null|*[]}
+ * 返回的数组
  */
 function buildQuestions(){
     let size = document.getElementById("inputNumber").value;
@@ -66,6 +114,9 @@ function buildQuestions(){
     let res = [];
     for(let i =0;i<size;i++){
         let A = new Question();
+        while (A.isOutOfEdge()){
+            A = new Question();
+        }
         let flag = false;
         for(let j=0;j<i;j++){
             if (isTwoQuestionsEqual(A,res[j])){
@@ -80,16 +131,18 @@ function buildQuestions(){
         }
     }
     window.alert("The questions have been spawned successfully.");
-    let buttonQuestion = document.getElementById("spawnQuestionFile");
-    let buttonAnswer = document.getElementById("spawnAnswerFile");
-    buttonAnswer.disabled = false;
-    buttonQuestion.disabled = false;
+    let buttonQuestion = document.getElementsByClassName("downloadButton");
+    for (let buttonQuestionKey of buttonQuestion) {
+        buttonQuestionKey.disabled = false;
+    }
     return res;
 }
 
 /**
- * @description 下载算式文件
- * @param questions 传进来的question数组
+ * @function
+ * 下载算式文件
+ * @param questions
+ * 传进来的question数组
  */
 function printQuestions(questions){
     let size = questions.length;
@@ -105,14 +158,16 @@ function printQuestions(questions){
 }
 
 /**
- * @description 下载答案文件
- * @param questions 传进来的question数组
+ * @function
+ * 下载答案文件
+ * @param questions
+ * 传进来的question数组
  */
 function printAnswers(questions){
     let size = questions.length;
     let stringAnswers = [];
     for(let i =0;i<size;i++){
-        stringAnswers[i] = (i+1) +". "+ questions[i].toString() + questions[i].result+ "\n";
+        stringAnswers[i] = (i+1) +". "+ questions[i].toString() + "=" +questions[i].totalResult + "\n";
     }
     let txtFile = new File(stringAnswers,"Answers.txt");
     let link = document.createElement('a');
